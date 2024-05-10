ad1ea7fcb96ffd245a912cf4aaec758067d88fef
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-10 16:47:52 +0530

    Variabes and Datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,20 @@
+const Id=1234
+let Email="hk@gmail.com"
+var Password="1234";
+City="Raipur" //not suggested
+let State;
+
+
+// Id=235 //not allowed
+
+console.log(Id); 
+
+Email="hk123@gmail.com"
+Password="678";
+City="CG"
+
+console.table([Id,Email,Password,City,State])
+
+/*
+Prefer not to use var
+*/
