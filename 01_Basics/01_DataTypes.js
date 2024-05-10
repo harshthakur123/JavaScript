c61f294ef5ff4581b8759f660d36f7ffe58dacc1
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-10 17:02:11 +0530

    DataTypes

---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+console.log("HK")
+
+//Premitive Datatype 
+
+let name="HK"  //String
+let age=23     //int
+let isLoggedIn=false    //false
+// null -> standalone value ; representation of empty value
+// undefined -> unasigned value
+// symbol -> unique
+
+
+// Reference DataType
+
+// object
+console.log(typeof name)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
