223091d70e323a44198d01908c25163add3fa5ad
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-13 14:11:05 +0530

    conversion of datatypes

---
+++ 01_conversionOperators.js
@@ -0,0 +1,44 @@
+// let score1=33
+// let score2="33"
+// let score3="33abc"
+
+// console.log(typeof(score1))
+// console.log(typeof(score2))
+// console.log(typeof(score3))
+
+// let valueInNumber1=Number(score2)
+// let valueInNumber2=Number(score3)
+
+// console.log(valueInNumber1)
+// console.log(valueInNumber2)
+
+// let b1=true
+// let b2=false
+
+// console.log(b1,b2)
+
+// let score4="HK"
+// let valueInNumber3=Number(score4)
+
+// console.log(valueInNumber3)
+// console.log(NaN)
+//33=>33
+//"33"=>33
+//"44ascas"=>NaN(Not a number)=>datatype
+
+// let isLoggedIn=1
+// console.log(isLoggedIn)
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let strBool1=""
+// let strBool2="HK"
+// let isBool1=Boolean(strBool1)
+// let isBool2=Boolean(strBool2)
+// console.log(isBool1)
+// console.log(isBool2)
+
+
+let num1=33
+let str1=String(num1)
+console.log(typeof str1)
