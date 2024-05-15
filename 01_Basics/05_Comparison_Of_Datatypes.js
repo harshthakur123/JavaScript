f319a67048ca749b68f65dd6bcbab3dfd248c598
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-15 21:19:34 +0530

    DataType Comparison

---
+++ 01_Basics/05_Comparison_Of_Datatypes.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+console.log("--------------------------");
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) //comparison converts nul to a number,treating it as 0.
+//Thts why null>=0 is true while null>0 is false
+console.log("---------------------------");
+console.log("2"==2)
+console.log("2"===2);
