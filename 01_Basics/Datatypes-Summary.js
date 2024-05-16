426f5b1cf112d821a449dbe30b513f3aca1b94cd
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-16 12:37:55 +0530

    Strings

--- 01_Basics/Datatypes-Summary.js
+++ 01_Basics/Datatypes-Summary.js
@@ -4,28 +4,46 @@
 String,Number,Boolean,Null,Undefined,Symbol(To define unique value)
 */
 
-const id1=Symbol('123')
-const id2=Symbol('123')
-console.log(id1===id2);
+// const id1=Symbol('123')
+// const id2=Symbol('123')
+// console.log(id1===id2);
 
 
-//Reference Datatypes
-/*
-Array,Object,Function
-*/
+// //Reference Datatypes
+// /*
+// Array,Object,Function
+// */
 
-const num=["one","two","three"]
+// const num=["one","two","three"]
 
-let myObj={
+// let myObj={
+//     name:"HK",
+//     age:"22"
+// }
+
+// const myFunction=function(){
+//     console.log("Hello World");
+// }
+// myFunction();
+
+// console.log(typeof myFunction);
+// console.log(typeof myObj);
+// console.log(typeof num);
+
+//---------------------------------------------------------------
+
+//Stack (Primitive), Heap(Non-Primitive)
+
+let name1="HK"
+let name2=name1
+name2="PK"
+console.log(name1,name2);
+
+let myObj1={
     name:"HK",
-    age:"22"
+    email:"hk@gmail.com"
 }
 
-const myFunction=function(){
-    console.log("Hello World");
-}
-myFunction();
-
-console.log(typeof myFunction);
-console.log(typeof myObj);
-console.log(typeof num);
+let myObj2=myObj1
+myObj2.email="hk@google.com"
+console.log(myObj1.email,myObj2.email);
---
+++ 06_Strings.js
@@ -0,0 +1,24 @@
+// const name="HK"
+// const repoCount=50
+
+// console.log(`Hello my name is ${name}`); //String interpolation
+
+// const email=new String('hk@gmai-l.com')
+// console.log(email.indexOf("@"));
+// console.log(email.length);
+// console.log(email.toUpperCase());
+// console.log(email.slice(1,4));
+// console.log(email.substring(0,4));
+
+
+const newString="    helolo   "
+console.log(newString );
+console.log(newString.trim());
+
+const url="https://harsh.com/harsh%20thakur"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('harsh'));
+
+const str1="My name is anthony gol service"
+console.log(str1.split(" "));
