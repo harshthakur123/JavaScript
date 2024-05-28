b52f17af06d1b3e0643dc5bceab645251b935233
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-28 18:18:16 +0530

    switch-case

---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,42 @@
+// switch(key){
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+
+switch (month) {
+
+    // default:
+    //     console.log("Default");
+    //     break;
+
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+
+     default:
+            console.log("Default");
+            break;
+
+   
+}
