3bfcfbaf99431986ee736bd0e063116b1f23712d
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-29 18:04:23 +0530

    Iterations-For-Of-Loop

---
+++ 05_Iterations.js/03_For-off-loop.js
@@ -0,0 +1,49 @@
+// for - in
+
+// const arr1=[1,2,3,4,5]
+
+// for (const num of arr1) {
+//     console.log(num);
+// }
+
+//----------------------------------------------------------
+
+//for - of
+
+// const greetings="Hello Harsh"
+
+// for (const greet of greetings) {
+//     console.log(`Each caharcter is ${greet}`);
+// }
+
+//-------------------------------------------------------------
+
+const map=new Map()
+ map.set('IN',"India")
+ map.set('USA',"Unided States Of America")
+ map.set('FR',"France")
+ map.set('IN',"India")
+
+ //  console.log(map);
+
+//  for (const key of map) {
+//     console.log(key);
+//  }
+
+// for (const [key,value] of map) {
+//     console.log(key,":-",value);
+//  }
+
+//---------------------------------------
+//  const game={
+//     'game1':'NFS',
+//     'game2':'Spiderman',
+
+//  } 
+
+// for (const [key,value] of game) {
+//     console.log(key,':-',value);
+// }
+//---------------------------------------Objects are not iteratable
+
+
