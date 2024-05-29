b6f494e83d66a8fd4dcc00a5b73c097a96b4e68e
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-29 18:10:30 +0530

    iterations-for-in-loop

---
+++ 05_Iterations.js/04_Objects.js
@@ -0,0 +1,35 @@
+// For - in - loop ---> for in loop extract keys by default
+
+// const language={
+//     js:"JavaScript",
+//     cpp:"C++",
+//     rb:"Ruby",
+//     swift:"swift by apple"
+// }
+
+// console.log(language['js']);
+
+// for (const key in language) {
+//    console.log(`${key} shortcut is for ${language[key]} `);
+// }
+
+
+// const lang=['c','cpp','java','js','python']
+
+// //keys in array --> 0,1,2,3,4........
+
+// for (const key in lang) {
+//     console.log(lang[key]);
+// }
+
+
+
+// const map=new Map()
+//  map.set('IN',"India")
+//  map.set('USA',"Unided States Of America")
+//  map.set('FR',"France")
+//  map.set('IN',"India")
+
+//  for (const key in map) {
+//     console.log(key);
+//  }
