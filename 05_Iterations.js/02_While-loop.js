d52c8844e8448e246d2846c5f873828f2ecb65cd
harshthakur123 <thakurharshsingh99@gmail.com>
2024-05-29 18:03:44 +0530

    Iterations-While-loop

---
+++ 05_Iterations.js/02_While-loop.js
@@ -0,0 +1,26 @@
+// let index=1;
+
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     index+=2;
+
+// }
+
+// let arr1=["flash","hulk","pepe","kaka"]
+
+// let index=0
+// while (index<arr1.length) {
+//     console.log(`Value is ${arr1[index]}`);
+//     index=index+1
+// }
+
+
+// Do- While
+
+// let score = 1
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
